4c0bd6a450bb74b90cb876929d98e94410836ca7
hughrawlinson <hugh@codeoclock.net>
2015-02-23 01:00:14 +0000

    refactored code to use the 500px npm package

--- photoPropertyHistogramByUser.js
+++ photoPropertyHistogramByUser.js
@@ -1,12 +1,53 @@
 var request = require('request');
-
 var histogram = {};
 var username = process.argv[2];
 var property = process.argv[3];
 
-var validProperties = ["id","user_id","name","description","camera","lens","focal_length","iso","shutter_speed","aperture","times_viewed","rating","status","created_at","category","location","latitude","longitude","taken_at","hi_res_uploaded","for_sale","width","height","votes_count","favorites_count","comments_count","nsfw","sales_count","for_sale_date","highest_rating","highest_rating_date","license_type","converted","collections_count","crop_version","privacy","image_url","url","positive_votes_count","converted_bits","image_format"];
-
 // validation
+var validProperties = [
+	"id",
+	"user_id",
+	"name",
+	"description",
+	"camera",
+	"lens",
+	"focal_length",
+	"iso",
+	"shutter_speed",
+	"aperture",
+	"times_viewed",
+	"rating",
+	"status",
+	"created_at",
+	"category",
+	"location",
+	"latitude",
+	"longitude",
+	"taken_at",
+	"hi_res_uploaded",
+	"for_sale",
+	"width",
+	"height",
+	"votes_count",
+	"favorites_count",
+	"comments_count",
+	"nsfw",
+	"sales_count",
+	"for_sale_date",
+	"highest_rating",
+	"highest_rating_date",
+	"license_type",
+	"converted",
+	"collections_count",
+	"crop_version",
+	"privacy",
+	"image_url",
+	"url",
+	"positive_votes_count",
+	"converted_bits",
+	"image_format"
+];
+
 if(process.env.FIVE_HUNDRED_PIXELS_KEY===null){
 	console.error("Please put your 500px Application key in your environmental variables as \"FIVE_HUNDRED_PIXELS_KEY\"");
 	process.exit();
@@ -20,29 +61,27 @@ if(validProperties.indexOf(property)==-1){
 	process.exit();
 }
 
-var maxpage = 1000;
+var API500px = require('500px'),
+    api500px = new API500px(process.env.FIVE_HUNDRED_PIXELS_KEY);
 
 function run(page){
-	if(page <= maxpage){
-		request("https://api.500px.com/v1/photos?feature=user&username="+username+"&consumer_key="+process.env.FIVE_HUNDRED_PIXELS_KEY+"&page="+page,
-			function(b,a){
-				a = JSON.parse(a.body);
-				if(a.photos !== undefined){
-					maxpage = a.total_pages;
-					for(var i = 0; i < a.photos.length; i++){
-						histogram[a.photos[i][property]] = histogram[a.photos[i][property]]+1 || 1; 
-					}
-					run(page+1);
-				}
-				else{
-					console.error(a);
-					process.exit();
-				}
-			});
-	}
-	else{
-		console.log(histogram);
-	}
+	api500px.photos.getByUsername(username,{page:page},function(error,results){
+		if(error){
+			console.error(error);
+			process.exit();
+		}
+		else{
+			for(var i = 0; i < results.photos.length;i++){
+				histogram[results.photos[i][property]] = histogram[results.photos[i][property]]+1 || 1;
+			}
+			if(page < results.total_pages){
+				run(page+1);
+			}
+			else{
+				console.log(histogram);
+			}
+		}
+	});
 }
 
 run(1);
